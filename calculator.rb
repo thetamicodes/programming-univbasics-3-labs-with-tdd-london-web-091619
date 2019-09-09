85b860fa77116608c67e2eaf8477e22ca346bc4f
Tami Reichold <reichold.tamara@gmail.com>
2019-09-09 16:30:45 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,6 @@
 first_number = 1.floor; # Add your variables here
 second_number > 0.floor;
 sum = first_number + second_number;
+difference = first_number - second_number;
+product = first_number * second_number;
+quotient = first_number / second_number;
