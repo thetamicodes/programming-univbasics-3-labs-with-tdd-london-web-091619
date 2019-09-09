0f62d4cb794fb07a4e8ff09ed4176ffd2f83fd3e
Tami Reichold <reichold.tamara@gmail.com>
2019-09-09 16:35:26 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,5 @@
-first_number >= 0; # Add your variables here
-second_number > 0.floor;
+first_number = 2; # Add your variables here
+second_number = 1;
 sum = first_number + second_number;
 difference = first_number - second_number;
 product = first_number * second_number;
